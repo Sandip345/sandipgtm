30f8d0ffdcd1edad1a78f8afa25a4da2e9108c67
Sandip345 <sandipgautamm787@gmail.com>
2026-02-07 15:31:49 -0500

    update 17

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,7 @@
 
 export default new Map([
-["src/content/projects/axial-flow-compressor-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Faxial-flow-compressor-design.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/axial-flow-compressor-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Faxial-flow-compressor-design.mdx&astroContentModuleFlag=true")],
+["src/content/projects/bijuli-powerline-inspection-uav.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbijuli-powerline-inspection-uav.mdx&astroContentModuleFlag=true")],
+["src/content/projects/bachelor_thesis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbachelor_thesis.mdx&astroContentModuleFlag=true")],
+["src/content/projects/aircraft-exhaust-stack-trolley.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Faircraft-exhaust-stack-trolley.mdx&astroContentModuleFlag=true")]]);
 		
