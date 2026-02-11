f5cd967ebc90de4cd8c35100270cabb41b2c8c42
Sandip345 <sandipgautamm787@gmail.com>
2026-02-11 01:04:16 -0500

    update 20

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
 ["src/content/projects/axial-flow-compressor-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Faxial-flow-compressor-design.mdx&astroContentModuleFlag=true")],
+["src/content/projects/lift-drag-data-acquisition.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Flift-drag-data-acquisition.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/bachelor_thesis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbachelor_thesis.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/bijuli-powerline-inspection-uav.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbijuli-powerline-inspection-uav.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/aircraft-exhaust-stack-trolley.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Faircraft-exhaust-stack-trolley.mdx&astroContentModuleFlag=true")],
-["src/content/projects/thermal-vacuum-chamber-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fthermal-vacuum-chamber-design.mdx&astroContentModuleFlag=true")],
-["src/content/projects/lift-drag-data-acquisition.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Flift-drag-data-acquisition.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/thermal-vacuum-chamber-design.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fthermal-vacuum-chamber-design.mdx&astroContentModuleFlag=true")]]);
 		
